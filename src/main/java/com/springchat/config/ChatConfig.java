ee759a222de3adff9034c409b1f6bbbc25f0e201
Meriç Cana <mmericcana@gmail.com>
2024-03-17 20:00:07 +0300

    update

--- src/main/java/com/springchat/config/ChatConfig.java
+++ src/main/java/com/springchat/config/ChatConfig.java
@@ -58,7 +58,7 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("https://spring-chat-ui.vercel.app").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
